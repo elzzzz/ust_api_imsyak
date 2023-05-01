0bc84e86ee8ca2bada11b1a8814085d2ee75a8d1
elzzzz <wathi2687@gmail.com>
2023-05-01 21:20:31 +0800

    Update Jadwal

--- utsApiApp/serializers.py
+++ utsApiApp/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class jadwalSerializer(serializers.ModelSerializer):
     class Meta:
         model = jadwalModels
-        fields = ['Tanggal','Imsyak','Subuh','Terbit','Duha','Zuhur','Asar','Magrib','Isya']
+        fields = ['id','Tanggal','Imsyak','Subuh','Terbit','Duha','Zuhur','Asar','Magrib','Isya']
